1a4d0ad17a70f303c1e088843517361a9c795a76
Agata Sumowska <agata.sumowska@gmail.com>
2020-05-05 20:48:32 +0200

    checked death count is display

--- AcceptanceTest.py
+++ AcceptanceTest.py
@@ -15,13 +15,13 @@ class MyTest(LiveServerTestCase):
 
     def get_today_as_string(self):
         tz = timezone('US/Eastern')
-        date = datetime.now(tz).strftime('%Y%m%d')
+        date = datetime.now(tz).strftime('%m %d %Y')
         return date
 
     def get_yesterday_as_string(self):
         tz = timezone('US/Eastern')
         yesterday = datetime.now(tz) - timedelta(days=1)
-        return yesterday.strftime('%Y%m%d')
+        return yesterday.strftime('%m %d %Y')
 
     def test_getting_historical_data_for_death_count_from_california(self):
         #Given
@@ -35,14 +35,14 @@ class MyTest(LiveServerTestCase):
         today_string = self.get_today_as_string()
         yesterday_string = self.get_yesterday_as_string()
         #Then
-        latest_date = parsed_html.find(id='row1_date')
-        latest_death_count = parsed_html.find(id='row1_death_count').get_text()
-        print(latest_death_count)
+        latest_date = parsed_html.find(id='row1_date').get_text()
+        latest_death_count = int(parsed_html.find(id='row1_death_count').get_text())
+
         header = parsed_html.find(id='state').get_text()
         #Then
         self.assertIn(header, expected_header)
         self.assertIn(latest_date, [today_string, yesterday_string])
-        # self.assertTrue(isinstance(latest_death_count, int))
+        self.assertTrue(isinstance(latest_death_count, int))
 
 if __name__ == '__main__':
     unittest.main()
