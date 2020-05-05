717ee64d8372a3d80d3f029055b2adcc252f4615
Agata Sumowska <agata.sumowska@gmail.com>
2020-05-06 00:23:56 +0200

    data processor handles scenario when there is a day with no death

--- DataProcessorTest.py
+++ DataProcessorTest.py
@@ -17,7 +17,17 @@ class TestStringMethods(unittest.TestCase):
         result = processor.process(fake_data)
         self.assertEqual(expected_dictionary, result)
 
-
+    def test_data_processor_extracts_the_death_count_per_day_from_list_of_days_when_some_days_have_no_death(self):
+        fake_data = MockCovidApi .call_api(self)
+        expected_dictionary = [{'date': 20200504, 'death_count': 2254},
+                               {'date': 20200503, 'death_count': 2215},
+                               {'date': 20200502, 'death_count': 2171},
+                               {'date': 20200501, 'death_count': 2073},
+                               {'date': 20200430, 'death_count': 1982},
+                               {'date': 20200304, 'death_count': 0}]
+        processor = DataProcessor.DataProcessor()
+        result = processor.process(fake_data)
+        self.assertEqual(expected_dictionary, result)
 
 if __name__ == '__main__':
     unittest.main()
