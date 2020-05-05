9413d1487c4cda9c65cf44ba6fa6e3e1d736fe7d
Agata Sumowska <agata.sumowska@gmail.com>
2020-05-05 20:13:47 +0200

    verify that we are using the right template

--- unit_test.py
+++ unit_test.py
@@ -1,49 +1,34 @@
-import urllib3
 import unittest
-from datetime import datetime, timedelta
-from pytz import timezone
-from bs4 import BeautifulSoup
-from flask_testing import LiveServerTestCase
+from flask import template_rendered
+from contextlib import contextmanager
+from flask_testing import TestCase
 from covidtracker.__init__ import create_app
 
-class MyTest(LiveServerTestCase):
+class AppTest(TestCase):
 
     def create_app(test_config=None):
         app = create_app()
         return app
 
-    def get_today_as_string(self):
-        tz = timezone('US/Eastern')
-        date = datetime.now(tz).strftime('%Y%m%d')
-        return date
+    @contextmanager
+    def captured_templates(self, app):
+        recorded = []
 
-    def get_yesterday_as_string(self):
-        tz = timezone('US/Eastern')
-        yesterday = datetime.now(tz) - timedelta(days=1)
-        return yesterday.strftime('%Y%m%d')
+        def record(sender, template, context, **extra):
+            recorded.append((template, context))
+        template_rendered.connect(record, app)
+        try:
+            yield recorded
+        finally:
+            template_rendered.disconnect(record, app)
 
     def test_server_retrieves_californian_historical_data_for_death_count_including_today_or_yesterday(self):
-        #Given
-        # 'http://localhost:5000'
-        url = self.get_server_url()
-
-        http = urllib3.PoolManager()
-        # response = http.request('GET', url)
-        response = http.request(url=url, method='GET')
-        html = response.data
-        print(html)
-        parsed_html = BeautifulSoup(html)
-        expected_header = "California"
-        today_string = self.get_today_as_string()
-        yesterday_string = self.get_yesterday_as_string()
-        #Then
-        latest_date = parsed_html.find(id='row1_date')
-        latest_death_count = parsed_html.find(id='row1_death_count')
-        header = parsed_html.find(id='state')
-        #Then
-        self.assertEqual(expected_header, header)
-        self.assertIn(latest_date, [today_string, yesterday_string])
-        self.assertTrue(isinstance(latest_death_count, int))
+       with self.captured_templates(self.app) as templates:
+            rv = self.app.test_client().get('/')
+            assert rv.status_code == 200
+            assert len(templates) == 1
+            template, context = templates[0]
+            assert template.name == 'index.html'
 
 if __name__ == '__main__':
     unittest.main()
