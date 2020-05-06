85545ade2cb381a1b3e13887d1cf968bcd7cc390
Agata Sumowska <agata.sumowska@gmail.com>
2020-05-06 02:20:07 +0200

    revert extracting data processor to a separate class

--- DataProcessorTest.py
+++ DataProcessorTest.py
@@ -1,5 +1,5 @@
 import unittest
-from covidtracker.service import DataProcessor
+from covidtracker.app import DataProcessor
 from MockCovidApi import MockCovidApi
 
 class TestStringMethods(unittest.TestCase):
@@ -13,19 +13,19 @@ class TestStringMethods(unittest.TestCase):
                                {'date': '05 01 2020', 'death_count': 2073},
                                {'date': '04 30 2020', 'death_count': 1982},
                                {'date': '03 04 2020', 'death_count': 0}]
-        processor = app.DataProcessor()
+        processor = DataProcessor()
         result = processor.process(fake_data)
         self.assertEqual(expected_dictionary, result)
 
     def test_data_processor_extracts_the_death_count_per_day_from_list_of_days_when_some_days_have_no_death(self):
         fake_data = MockCovidApi .call_api(self)
-        expected_dictionary = [{'date': 20200504, 'death_count': 2254},
-                               {'date': 20200503, 'death_count': 2215},
-                               {'date': 20200502, 'death_count': 2171},
-                               {'date': 20200501, 'death_count': 2073},
-                               {'date': 20200430, 'death_count': 1982},
-                               {'date': 20200304, 'death_count': 0}]
-        processor = DataProcessor.DataProcessor()
+        expected_dictionary = [{'date': '05 04 2020', 'death_count': 2254},
+                               {'date': '05 03 2020', 'death_count': 2215},
+                               {'date': '05 02 2020', 'death_count': 2171},
+                               {'date': '05 01 2020', 'death_count': 2073},
+                               {'date': '04 30 2020', 'death_count': 1982},
+                               {'date': '03 04 2020', 'death_count': 0}]
+        processor = DataProcessor()
         result = processor.process(fake_data)
         self.assertEqual(expected_dictionary, result)
 
--- covidtracker/app.py
+++ covidtracker/app.py
@@ -2,8 +2,28 @@ from flask import Flask, render_template
 from flask_cors import CORS
 import urllib3
 import json
-from covidtracker.service import DataProcessor
 
+import datetime
+
+
+class DataProcessor:
+
+    def process(self, list_of_days):
+        result = list(map(
+            lambda day: {"date": self._format_date_for_display(day["date"]), "death_count": self._get_death_key(day)},
+            list_of_days))
+        return result
+
+    def _format_date_for_display(self, date_as_int):
+        unformated_date = str(date_as_int)
+        formated_date = datetime.datetime.strptime(unformated_date, '%Y%m%d').strftime('%m %d %Y')
+        return formated_date
+
+    def _get_death_key(self, day_dictionary):
+        if "death" in day_dictionary:
+            return day_dictionary["death"]
+        else:
+            return 0
 
 class RealCovidApi:
 
@@ -22,7 +42,7 @@ def create_app(covid_api):
     @app.route('/')
     def index():
         data = covid_api.call_api()
-        processed_data = DataProcessor.DataProcessor().process(data)
+        processed_data = DataProcessor().process(data)
         return render_template('index.html', data=processed_data, enumerate=enumerate, str=str)
 
     return app
